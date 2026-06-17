0103dd1b478ef9fdd0c48cd4328967d8846c3cf6
Sergio M. <50914599+sergiomb04@users.noreply.github.com>
2026-06-17 15:16:28 +0000

    upload: working version

---
+++ my-app/app/formulario-sergiomb/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+
+interface FormData {
+    name: string, 
+    email: string,
+    subject: string,
+}
+
+const initialData: FormData = {
+    name: "",
+    email: "",
+    subject: "",
+}
+
+const FormularioSergioMBPage = () => {
+
+    const [data, setData] = useState<FormData>(initialData);
+
+    const onDataChange = (event: any) => {
+        const { id, value } = event.target;
+        //console.log("target:", event.target);
+        setData((prev) => ({
+            ...prev,
+            [id]: value,
+        }));
+    }
+    
+    const onSubmit = (event: any) => {
+        event.preventDefault();
+        console.log("Data to send:", data)
+    }
+
+    return (
+        <div className="flex flex-col justify-center align-center items-center">
+            <div>
+                <h1>Formulario</h1>
+                <h2>Formulario hecho a mano para prácticar estados de React.</h2>
+            </div>
+
+            <form className="flex flex-col w-2xl" onSubmit={onSubmit}>
+                <label htmlFor="name">Nombre</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={data.name}
+                    onChange={onDataChange}
+                    placeholder="Introduce nombre..." required
+                />
+
+                <label htmlFor="email">Correo</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={data.email}
+                    onChange={onDataChange}
+                    placeholder="Introduce correo..." required
+                />
+
+                <label htmlFor="subject">Asunto</label>
+                <textarea
+                    id="subject"
+                    placeholder="Introduce asunto..."
+                    value={data.subject}
+                    onChange={onDataChange}
+                    required
+                />
+
+
+                <button type="button" className="" value="Submit" onClick={onSubmit}>Submit</button>
+            </form>
+        </div>
+
+    );
+}
+
+export default FormularioSergioMBPage;
